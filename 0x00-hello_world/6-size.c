c571778985c5427c0a30ab108df6328e09998550
shorouk589 <shoroukashraf66@gmail.com>
2023-03-16 19:20:40 -0700

    asdfg

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 	printf("Size of a char: %lu butes(s)\n", sizeof(char));
 	printf("Size of an int: %lu butes(s)\n", sizeof(int));
 	printf("Size of a long int: %lu butes(s)\n", sizeof(long int));
-	printf("Size of a long long: %lu butes(s)\n", sizeof(long long));
+	printf("Size of a long long it: %lu butes(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu butes(s)\n", sizeof(float));
 	return (0);
 }
