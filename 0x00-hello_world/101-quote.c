a42d3a0f79bcfa883c14e784362c412e09302995
shorouk589 <shoroukashraf66@gmail.com>
2023-03-16 20:54:59 -0700

    asdfg

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print a quote using write fun
+ *	write(int fd.const void *buf.size_t count)
+ *	Return: 1 (fail)
+*/
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
+}
