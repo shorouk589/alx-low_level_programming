33a0b68067200b626199bc567241f1fa4b9284a1
shorouk589 <shoroukashraf66@gmail.com>
2023-03-16 19:17:45 -0700

    asdfg

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: uding sizeof to print the size of various types
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	printf("Size of a char: %lu butes(s)\n", sizeof(char));
+	printf("Size of an int: %lu butes(s)\n", sizeof(int));
+	printf("Size of a long int: %lu butes(s)\n", sizeof(long int));
+	printf("Size of a long long: %lu butes(s)\n", sizeof(long long));
+	printf("Size of a float: %lu butes(s)\n", sizeof(float));
+	return (0);
+}
