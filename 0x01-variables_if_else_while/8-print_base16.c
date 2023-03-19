527d31faa7600a89c6253a3e245926bea04c977f
shorouk589 <shoroukashraf66@gmail.com>
2023-03-18 17:42:02 -0700

    qasdfg

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,9 +12,9 @@ int main(void)
 	char j;
 
 	for (i = 0 ; i < 10 ; i++)
-		 putchar(i + '0');
+		putchar(i + '0');
 	for (j = 'a' ; j <= 'f' ; j++)
-		 putchar(j);
+		putchar(j);
 	putchar('\n');
 	return (0);
 }
