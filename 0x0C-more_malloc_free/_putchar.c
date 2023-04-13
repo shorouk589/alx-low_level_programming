680680916c957c09ed380cb0465cf33d14ac5bf4
shorouk589 <shoroukashraf66@gmail.com>
2023-04-13 16:19:46 -0700

    aasdf

---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writse the char c to stdout
+ * @c: the char to print
+ *
+ * Return: On success1.
+ * Onerror, -1 is returned, and error is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (wrie(1, &c, 1));
+}
