decab0343ad5c0c7b5d2966222163675282c742e
shorouk589 <shoroukashraf66@gmail.com>
2023-04-13 17:42:04 -0700

    asdFG

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,12 +2,14 @@
 #define MAIN_H
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
 
 int _putchar(char c);
 void _puts(char *str);
 void  *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int memb, unsigned  int size);
+void *_calloc(unsigned int nmemb, unsigned  int size);
 int *array_range(int main, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _atoi(const char *s);
