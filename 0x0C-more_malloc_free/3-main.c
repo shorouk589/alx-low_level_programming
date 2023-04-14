1f39fe8aad70f0a9e86ccfc7292619530935dea6
shorouk589 <shoroukashraf66@gmail.com>
2023-04-13 19:14:42 -0700

    asdf

--- 0x0C-more_malloc_free/3-main.c
+++ 0x0C-more_malloc_free/3-main.c
@@ -41,7 +41,7 @@ int main(void)
 	int *a;
 
 	a = array_range(0, 10);
-       simple_print_buffer(a, 11);
-       free(a);
-       return (0);
+	simple_print_buffer(a, 11);
+	free(a);
+	return (0);
 }
