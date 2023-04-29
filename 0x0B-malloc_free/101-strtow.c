2944130a28547caf37727135b093b6b73cb67fd3
shorouk589 <shoroukashraf66@gmail.com>
2023-04-29 03:39:08 -0700

    101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -30,7 +30,7 @@ int count_word(char *s)
  * **strtow - splits a string into words
  * @str: string to split
  *
- * Retuen: pointer to an array of strings (Success)
+ * Return: pointer to an array of strings (Success)
  * or NULL (Error)
  */
 char **strtow(char *str)
