d929a40053786ea32f51af98d5df2547c8659b00
shorouk589 <shoroukashraf66@gmail.com>
2023-05-01 11:43:32 -0700

    zx

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -70,6 +70,7 @@ size_t print_listint_safe(const listint_t *head)
 		{
 			printf("[%p] %d\n", (void *)head, head->n);
 		}
-		printf("-> [%p] %d\n", (void *)head, head->n);}
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	}
 	return (nodes);
 }
