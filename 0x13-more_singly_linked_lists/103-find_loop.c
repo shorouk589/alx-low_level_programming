7d73d4287115a7723c474441595b61d641f5a8bd
shorouk589 <shoroukashraf66@gmail.com>
2023-05-01 15:38:00 -0700

    103-finish

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -7,8 +7,8 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint *slow = head;
-	listint *fast = head;
+	listint_t *slow = head;
+	listint_t *fast = head;
 
 	if (!head)
 		return (NULL);
