7d5214b07ab373141e8429308dd6d9fdb4067e64
shorouk589 <shoroukashraf66@gmail.com>
2023-05-01 09:07:15 -0700

    101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -13,7 +13,7 @@ size_t print_listint_safe(const listint_t *head);
  */
 size_t looped_listint_len(const listint_t *head)
 {
-	const listint_t *tortoise, *here;
+	const listint_t *tortoise, *hare;
 	size_t nodes = 1;
 
 	if (head == NULL || head->next == NULL)
@@ -71,7 +71,7 @@ size_t print_listint_safe(const listint_t *head)
 		{
 			printf("[%p] %d\n", (void *)head, head->n);
 		}
-		printf("[%p] %d\n", (void * )head, head->n)
+		printf("[%p] %d\n", (void * )head, head->n);
 	}
 	return (nodes);
 }
