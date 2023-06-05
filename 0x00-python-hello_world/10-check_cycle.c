a529de734b0658d1c131ccc86afc57f9aeecfcc0
shorouk589 <shoroukashraf66@gmail.com>
2023-06-05 14:06:01 -0700

    all

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * check_cycle - check linked list contained a cycles
+ * @list: link
+ *
+ * Return: 1 if the list has a cycle success, 0 if it doesn't
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	if (!list)
+		return (0);
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
